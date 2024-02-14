90d4827fa771075015c5ccbabd7e21de75301c80
Daniel Amsel <lesmaleinad@hotmail.com>
2024-02-13 16:55:23 -0800

    Up max listeners

--- src/actions/listen.ts
+++ src/actions/listen.ts
@@ -68,6 +68,7 @@ export async function joinAndListen(
                     duration: 500,
                 },
             })
+            .setMaxListeners(50)
             .on('error', console.error)
             .pipe(decoder);
 
