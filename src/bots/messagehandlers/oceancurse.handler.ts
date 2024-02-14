361b44baf9a21973db28a3101f3f41266ff031ae
Daniel Amsel <lesmaleinad@hotmail.com>
2024-02-13 21:17:22 -0800

    Don't allow curses outside of voice chat

--- src/bots/messagehandlers/oceancurse.handler.ts
+++ src/bots/messagehandlers/oceancurse.handler.ts
@@ -29,23 +29,17 @@ export class OceanCurseHandler implements MessageHandler, VoiceStateHandler {
     ): Promise<boolean> {
         const channel = newState.channel;
 
-        // if (
-        //     oldState.channelId !== oceanCurse.defaultVoiceChannelId &&
-        //     channel?.id === oceanCurse.defaultVoiceChannelId &&
-        //     newState.member?.id === this.cursedMemberId
-        // ) {
-        //     await oceanCurse.sendToDefaultTextChannel(
-        //         `Deploying OceanCurse for ${newState.member.displayName}.`
-        //     );
-        //     oceanCurse.playOceanMan();
-        //     return true;
-        // }
-
         if (
             oldState.channelId !== oceanCurse.defaultVoiceChannelId &&
             channel?.id === oceanCurse.defaultVoiceChannelId &&
             newState.member?.id === this.cursedMemberId
         ) {
+            //     await oceanCurse.sendToDefaultTextChannel(
+            //         `Deploying OceanCurse for ${newState.member.displayName}.`
+            //     );
+            //     oceanCurse.playOceanMan();
+            //     return true;
+
             const voiceChannel = await oceanCurse.getDefaultVoiceChannel();
             joinAndListen(voiceChannel, newState.member.id, oceanCurse);
             return true;
@@ -83,10 +77,9 @@ export class OceanCurseHandler implements MessageHandler, VoiceStateHandler {
             }
         }
 
-        if (content.toLowerCase() === 'ocean curse') {
-            const requiredVoiceChannel =
-                await oceanCurse.getDefaultVoiceChannel();
+        const requiredVoiceChannel = await oceanCurse.getDefaultVoiceChannel();
 
+        if (content.toLowerCase() === 'ocean curse') {
             if (requiredVoiceChannel.members.has(author.id)) {
                 const cursedMember = await guild.members.fetch(
                     this.cursedMemberId
@@ -106,33 +99,39 @@ export class OceanCurseHandler implements MessageHandler, VoiceStateHandler {
             );
             oceanCurse.playOceanMan();
         } else {
-            try {
-                const curseInput = content.split(' ')[2];
-                if (!curseInput) {
-                    throw new Error('newCursedMember not specified');
-                }
-                const curseIsId = !!parseInt(curseInput);
-                const newCursedMember = curseIsId
-                    ? await guild.members.fetch({ user: curseInput })
-                    : (await guild.members.fetch()).find(
-                          (member) =>
-                              member.user.tag.toLowerCase() ===
-                              curseInput.toLowerCase()
-                      );
-                if (!newCursedMember) {
-                    throw new Error(
-                        `Cannot find member. input: ${curseInput}, curseIsId: ${curseIsId}`
+            if (requiredVoiceChannel.members.has(author.id)) {
+                try {
+                    const curseInput = content.split(' ')[2];
+                    if (!curseInput) {
+                        throw new Error('newCursedMember not specified');
+                    }
+                    const curseIsId = !!parseInt(curseInput);
+                    const newCursedMember = curseIsId
+                        ? await guild.members.fetch({ user: curseInput })
+                        : (await guild.members.fetch()).find(
+                              (member) =>
+                                  member.user.tag.toLowerCase() ===
+                                  curseInput.toLowerCase()
+                          );
+                    if (!newCursedMember) {
+                        throw new Error(
+                            `Cannot find member. input: ${curseInput}, curseIsId: ${curseIsId}`
+                        );
+                    }
+
+                    this.cursedMemberId = newCursedMember.user.id;
+                    await replyAndDelete(
+                        `Cursing ${newCursedMember.displayName}. Self destruct in 15 seconds.`
+                    );
+                } catch (e) {
+                    console.error(e);
+                    oceanCurse.sendToDefaultTextChannel(
+                        "That curse didn't work. Curses only work like this: 'ocean curse <id OR tagname>'"
                     );
                 }
-
-                this.cursedMemberId = newCursedMember.user.id;
+            } else {
                 await replyAndDelete(
-                    `Cursing ${newCursedMember.displayName}. Self destruct in 15 seconds.`
-                );
-            } catch (e) {
-                console.error(e);
-                oceanCurse.sendToDefaultTextChannel(
-                    "That curse didn't work. Curses only work like this: 'ocean curse <id OR tagname>'"
+                    'You must join the voice channel to curse someone. Self destruct in 15 seconds.'
                 );
             }
         }
